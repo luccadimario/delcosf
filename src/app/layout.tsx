12960ef22cd3a9b2a1c37559a4bef38fa8d9c2aa
Lucca D <luccadimario@gmail.com>
2025-06-22 17:05:19 -0400

    fixed thumbnail path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
         siteName: 'Delco Science Fair',
         images: [
             {
-                url: 'https://www.delcosciencefair.com/delcosfthumb.jpg',
+                url: 'https://www.delcosciencefair.org/delcosfthumb.jpg',
                 width: 1200,
                 height: 630,
                 alt: 'Delco Science Fair',
